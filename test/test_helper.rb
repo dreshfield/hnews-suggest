0ebd5bd294e0450494eae0b16db25a1bfcff9bf4
Greg Chapple <gregchapple1@gmail.com>
2014-06-12 21:08:18 +0100

    Initial commit

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require "bundler"
+
+Bundler.require
+Bundler.require :test
+
+ENV["APP_ENV"] = "test"
+
+require "config"
+
+DatabaseCleaner.strategy = :transaction
---
+++ test/unit/bootstrap_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BoostrapTest < MiniTest::Test
+    def test_bootstrap_test_suite
+        assert true
+    end
+end
