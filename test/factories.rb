ffa07f838329c132668d1a3d02aea0bf3b47446b
Greg Chapple <gregchapple1@gmail.com>
2014-06-17 13:41:58 +0100

    Include database cleaner helper, add title to factory

--- test/factories.rb
+++ test/factories.rb
@@ -1,10 +1,12 @@
 require "news/models"
 require "helpers/sequel"
+require "helpers/database_cleaner"
 
 FactoryGirl.define do
     factory :keyword
 
     factory :article do
+        title "Programmers block"
         url "http://gregchapple.com/programmers-block/"
     end
 end
