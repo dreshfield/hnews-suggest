7f8ab3acba36f812ec3c5c71b99c91eee8864d94
Greg Chapple <gregchapple1@gmail.com>
2014-06-23 13:52:13 +0100

    Move non-test db to home directory
    When running the application, connect to a database in the home
    directory. If this file doesn't exist, it will be created and have the
    existing migrations run against it. It will then be used as normal for
    all subsequent commands.
    
    Fixes #11

--- config/config.rb
+++ config/config.rb
@@ -1,1 +1,7 @@
-Sequel.connect "sqlite://db/#{ENV["APP_ENV"] || "development"}.sqlite3" 
+if ENV["APP_ENV"]
+    Sequel.connect "sqlite://db/#{ENV["APP_ENV"]}.sqlite3" 
+else
+    db_file = File.expand_path("~/.hnews/db.sqlite3")
+
+    Sequel.connect "sqlite://#{db_file}"
+end
